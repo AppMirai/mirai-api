631f4ab91bad427ac33edcff5de11dfa1419f8e3
faiz <mnurfaiz26@gmail.com>
2022-10-26 21:45:55 +0700

    buat class face

---
+++ api/facelearning.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import dlib
+
+from api.face import Face
+
+
+class FaceLearing(Face):
+    def __init__(self):
+        pass
--- api/views.py
+++ api/views.py
@@ -14,6 +14,9 @@ import cv2
 import numpy as np
 import dlib
 
+from .face import *
+from .image import *
+
 # Create your views here.
 #Comment 2
 
@@ -23,9 +26,16 @@ class ImageAPIView(generics.RetrieveAPIView):
     renderer_classes = [JPEGRender]
 
     def get(self, request, *args, **kwargs):
+        r = 255
+        g = 0
+        b = 0
+        
         queryset = Images.objects.get(uid=self.kwargs['uid']).images
         uid = str(Images.objects.get(uid=self.kwargs['uid']).uid)
-        #Start Bikin Class baru
+        
+        # start refactor
+
+        # start image process
         face_datasets = os.path.join(PROJECT_ROOT, 'shape_predictor_68_face_landmarks.dat')
         data_link = str(queryset)
         base_directory = str(MEDIA_ROOT)
@@ -41,34 +51,13 @@ class ImageAPIView(generics.RetrieveAPIView):
 
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor(face_datasets)
-
-        r = 255
-        g = 0
-        b = 0
-
-        def createBoxLips(img, points, scale=5, masked=False, cropped=True):
-            if masked:
-                mask = np.zeros_like(img)
-                mask = cv2.fillPoly(mask, [points], (255, 255, 255))
-
-            if cropped:
-                bbox = cv2.boundingRect(points)
-                x, y, w, h = bbox
-                imgCrop = img[y:y + h, x:x + w]
-                imgCrop = cv2.resize(imgCrop, (0, 0), None, scale, scale)
-                return imgCrop
-
-            else:
-                return mask
-
-        # foto
         original_image = image.copy()
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = detector(gray_image)
+        # end image process
 
+        # image filter
         for face in faces:
-            x1, y1 = face.left(), face.top()
-            x2, y2 = face.right(), face.bottom()
             landmarks = predictor(gray_image, face)
             my_points = []
             for n in range(68):
@@ -77,17 +66,37 @@ class ImageAPIView(generics.RetrieveAPIView):
                 my_points.append([x, y])
 
             my_points = np.array(my_points)
-            image_lips = createBoxLips(image, my_points[48:61], 2, masked=True, cropped=False)
+            lips = Face(48, 61, my_points)
+            lips.image = lips.createBoxLips(image, lips.point, 2, masked=True, cropped=False)
+            lips.masking(original_image, b, g, r)
 
-            image_color_lips = np.zeros_like(image_lips)
-            image_color_lips[:] = b, g, r
-            image_color_lips = cv2.bitwise_and(image_lips, image_color_lips)
-            image_color_lips = cv2.GaussianBlur(image_color_lips, (7, 7), 10)
-            image_color_lips = cv2.addWeighted(original_image, 1, image_color_lips, 0.4, 0)
-            image = image_color_lips
+            leftEyeBrow = Face(17, 22, my_points)
+            leftEyeBrow.image = lips.createBoxLips(image, leftEyeBrow.point, 2, masked=True, cropped=False)
+            leftEyeBrow.masking(original_image, b, g, r)
 
-        cv2.imwrite(link, image_color_lips)
-        #end bikin class baru
+            rightEyeBrow = Face(22, 27, my_points)
+            rightEyeBrow.image = lips.createBoxLips(image, rightEyeBrow.point, 2, masked=True, cropped=False)
+            rightEyeBrow.masking(leftEyeBrow.color, b, g, r)
+
+            leftEye = Face(36, 42, my_points)
+            leftEye.image = lips.createBoxLips(image, leftEye.point, 2, masked=True, cropped=False)
+            leftEye.masking(original_image, b, g, r)
+
+            rightEye = Face(42, 47, my_points)
+            rightEye.image = lips.createBoxLips(image, rightEye.point, 2, masked=True, cropped=False)
+            rightEye.masking(leftEye.color, b, g, r)
+
+            leftCheek = Face(2, 5, my_points)
+            leftCheek.image = lips.createBoxLips(image, leftCheek.point, 2, masked=True, cropped=False)
+            leftCheek.masking(original_image, b, g, r)
+
+            rightCheek = Face(12, 15, my_points)
+            rightCheek.image = lips.createBoxLips(image, rightCheek.point, 2, masked=True, cropped=False)
+            rightCheek.masking(leftCheek.color, b, g, r)
+        # end image filter
+
+        cv2.imwrite(link, lips.color)
+        # end refactor
 
         return Response(queryset, content_type='image/jpg')
 
